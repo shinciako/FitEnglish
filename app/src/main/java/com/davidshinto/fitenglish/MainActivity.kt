20744edff2abb9c27a1c3c9b36b983d4b6b2d5e0
shinciako <100320142+shinciako@users.noreply.github.com>
2023-03-04 15:51:58 +0100

    Selected icon fix, added back button functionality

--- app/src/main/java/com/davidshinto/fitenglish/MainActivity.kt
+++ app/src/main/java/com/davidshinto/fitenglish/MainActivity.kt
@@ -2,6 +2,8 @@ package com.davidshinto.fitenglish
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.forEach
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -10,9 +12,11 @@ import com.davidshinto.fitenglish.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationBarView
 
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +26,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupNav(){
-        val navView: BottomNavigationView = binding.navView
+        navView= binding.navView
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         val appBarConfiguration = AppBarConfiguration(
             setOf(
@@ -35,5 +39,25 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.labelVisibilityMode = NavigationBarView.LABEL_VISIBILITY_LABELED
         navView.setupWithNavController(navController)
+        setupSelectedFragmentIcon(navController)
+    }
+
+    private fun setupSelectedFragmentIcon(navController: NavController){
+        navController.addOnDestinationChangedListener(){ controller, destination, _ ->
+            if(destination.id != navView.selectedItemId){
+                controller.backQueue.asReversed().drop(1).forEach { entry ->
+                    navView.menu.forEach { item ->
+                        if (entry.destination.id == item.itemId) {
+                            item.isChecked = true
+                            return@addOnDestinationChangedListener
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host_fragment_activity_main).navigateUp()
     }
 }
