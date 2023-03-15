e95380e481d7f1d75e6faf09a2e3b1fbfc9b19a7
shinciako <100320142+shinciako@users.noreply.github.com>
2023-03-15 15:14:01 +0100

    Update data classes

--- app/src/main/java/com/davidshinto/fitenglish/Category.kt
+++ app/src/main/java/com/davidshinto/fitenglish/Category.kt
@@ -1,4 +1,4 @@
 package com.davidshinto.fitenglish
 
-data class Category(val id: Int, val name: String)
+data class Category(val id: Int, val name: String) : java.io.Serializable
 
--- app/src/main/java/com/davidshinto/fitenglish/Game.kt
+++ app/src/main/java/com/davidshinto/fitenglish/Game.kt
@@ -1,14 +1,18 @@
 package com.davidshinto.fitenglish
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import kotlinx.parcelize.RawValue
 
+@Parcelize
 data class Game(
     val id: Int,
-    val category: Category,
-    val questionsPerTest: Int,
+    val mode: Mode,
+    val category: @RawValue Category,
     val distance: Int,
     val distanceAfterTest: Int,
-    val mode: Mode
-)
+    val questionsPerTest: Int
+) : Parcelable
 
 enum class Mode {
     Match, Flash, Writer
